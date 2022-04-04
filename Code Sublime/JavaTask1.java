2e8a6593b32a303543c369f68364859bce96385c
WingerOfOwl <102946487+WingerOfOwl@users.noreply.github.com>
2022-04-04 15:31:21 +0700

    Add files via upload

---
+++ Code Sublime/JavaTask1.java
@@ -0,0 +1,20 @@
+package taskjava1;
+
+ {
+
+   
+    public static void main(String[] args) {
+   
+        Biodata bio= new Biodata();
+        
+        bio.getFullName("Ahmad Muharam Jiddan Wahono");
+        bio.getPlaceBirth("Jakarta");
+        bio.getDateBirth(17," Maret ",2003);
+        bio.getHobby("Archery");
+        bio.getZodiac("Pieces");
+        bio.getFavFood("Fried Rice");
+        bio.getDreams("Software Engineer");
+        
+    }
+    
+}
